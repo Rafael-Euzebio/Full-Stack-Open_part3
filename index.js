94b4f926c591563485649759ae8ce49f765606c8
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2024-02-02 19:25:01 -0300

    3 - phonebook backend - receiving delete requests on /api/persons/:id

--- index.js
+++ index.js
@@ -62,6 +62,19 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => {
+    return person.id === id
+  })
+
+  if (person) {
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
